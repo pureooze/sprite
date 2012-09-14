4464da876db035fd4fc21e0cb91d2d9e4fc8cc5e
Alan Bortu <alanbortu@gmail.com>
2012-09-13 21:08:37 -0400

    Sprite class finished

--- src/Sprite.java
+++ src/Sprite.java
@@ -11,6 +11,85 @@ public class Sprite {
 	//CONSTRUCTOR
 	public Sprite(Animation animated){
 		
+		this.animated = animated;
+	}
+
+	//change position of sprite
+	public void update(long timePassed){
+		
+		x += velX * timePassed;
+		y += velY * timePassed;
+		animated.update(timePassed);
 	}
 	
+	//get x position
+	public float getX(){
+		
+		return x;
+	}
+	
+	//get y position
+	public float getY(){	
+		
+		return y;
+	}
+	
+	//set sprite x position
+	public void setX(float x){
+		
+		this.x = x;
+	}
+	
+	//set sprite y position
+	public void setY(float y){
+			
+		this.y = y;
+	}
+	
+	//get sprite width
+	public int getWidth(){
+		
+		return animated.getImage().getWidth(null);
+	}
+	
+	//get sprite height
+	public int getHeight(){
+			
+		return animated.getImage().getHeight(null);
+	}
+	
+	//get horizontal velocity
+	public float getVelocityX(){
+		
+		return velX;
+	}
+	
+	//get vertical velocity
+	public float getVelocityY(){
+			
+		return velY;
+	}
+	
+	//set horizontal velocity
+	public void setVelocityX(float velX){
+		
+		this.velX = velX;
+	}
+	
+	//set vertical velocity
+	public void setVelocityY(float velY){
+			
+		this.velY = velY;
+	}
+	
+	//get sprite
+	public Image getImage(){
+		
+		return animated.getImage();
+	}
+	
+	
 }
+
+
+
